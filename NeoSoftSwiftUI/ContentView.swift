6b1d6ea580c6007c0de768b399b4330b716e3623
aniketsingh55 <aniketsingh55577@gmail.com>
2025-05-06 03:28:58 +0530

    Initial Commit

---
+++ NeoSoftSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NeoSoftSwiftUI
+//
+//  Created by AdminTech02 on 06/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NeoSoftSwiftUI/NeoSoftSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoSoftSwiftUIApp.swift
+//  NeoSoftSwiftUI
+//
+//  Created by AdminTech02 on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NeoSoftSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
